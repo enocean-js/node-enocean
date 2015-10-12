d9767c6bb9ab04ee9e380d608c31babe2d2bb7fd
Holger Will <h.will@klimapartner.de>
2015-10-12 21:20:28 +0200

    added example for simple switch

--- examples/rockerSwitch.js
+++ examples/rockerSwitch.js
@@ -1,8 +1,12 @@
 var enocean      = require("../");  // require node-enocen
 
-
 enocean.listen("/dev/ttyUSB0");     // start listening to the serialport
 
 enocean.on("data",function(data){   // a telegram has been received
-	console.log(data)               // log it to the console
+	if(data.packetTypeString=="RPS" && data.state=="down") {
+		console.log("Button "+ data.button+ " on Sensor "+ data.senderId +" was pressed")
+	}
+	if(data.packetTypeString=="RPS" && data.state=="up") {
+		console.log("a Button on Sensor "+ data.senderId +" was released")
+	}
 });
