8759518b7116f84bf6080bc38dc1d6188295a859
Holger Will <h.will@klimapartner.de>
2015-10-14 20:23:31 +0200

    added example fro reading the base adress

---
+++ examples/getBase.js
@@ -0,0 +1,9 @@
+var enocean      = require("../");  
+enocean.listen("/dev/ttyUSB0"); 
+enocean.on("ready",function(data){   
+	enocean.getBase()            
+});
+enocean.on("base",function(data){   
+	console.log(data)  
+	enocean.close()             
+});
