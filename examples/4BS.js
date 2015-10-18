96887343ff3583b7ca33729b68ce4743be10788a
Holger Will <h.will@klimapartner.de>
2015-10-18 17:02:48 +0200

    example for adding an eep resolver

--- examples/4BS.js
+++ examples/4BS.js
@@ -1,3 +1,10 @@
 var enocean      = require("../")();  // require node-enocen
-
-console.log(enocean.getData("a5-04-03","8003f001"))
+enocean.eepResolvers.push(function(eep,data){
+	if(eep==="a5-99-99") {
+		return {
+			type:"custom",value:"Hello World!"+data.substring(0,2)
+		}
+	}
+	return null
+})
+console.log(enocean.getData("a5-99-99","8003f001"))
