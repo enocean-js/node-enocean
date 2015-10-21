0fe13bd97f6ad97ad9bf6b4055f4065e06255f62
Holger Will <h.will@klimapartner.de>
2015-10-21 13:52:04 +0200

    remove getBase examples as ists not needed any more

--- examples/getBase.js
+++
@@ -1,9 +0,0 @@
-var enocean      = require("../")();  
-enocean.listen("/dev/ttyUSB0"); 
-enocean.on("ready",function(data){   
-	enocean.getBase()            
-});
-enocean.on("base",function(data){   
-	console.log(data)  
-	enocean.close()             
-});
