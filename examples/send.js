c27610712a2e9958c8a9616c836ebd011cf7d982
Holger Will <h.will@klimapartner.de>
2015-10-22 20:55:30 +0200

    use the dev dependencies button and dimmer

--- examples/send.js
+++ examples/send.js
@@ -1,16 +1,16 @@
-var enocean = require("../")();           // require node-enocean
-var Button = require("../node_modules/node-enocean-button");           // require node-enocean
+// run this file from the command line with a paramter
+// "node send.js on" sends a switch on command. it can b eused as a teach in command
+// "node send.js off" sends a switch off command
+var enocean = require( "../" )( )             // require node-enocean
+var Button = require( "node-enocean-button" ) 
+enocean.listen( "/dev/ttyUSB0" );              // open the serialport
 
-enocean.listen("/dev/ttyUSB0");              // open the serialport
-enocean.on("ready",function(base){           // when ready
-
-	var button = new Button(enocean,1)
-
-
-	if(process.argv[2] == "on"){
-		button.A1.click()
+enocean.on( "ready" , function( base ) {           // when ready
+	var button = new Button( enocean , 1 )       // use enocean to sen on chanal 1
+	if(p rocess.argv[ 2 ] == "on" ) {             
+		button.A1.click( )
 	}else{
-		button.A0.click()	
+		button.A0.click( )	
 	}
-	setTimeout(function(){enocean.close()},200)
+	enocean.close( )
 });
--- examples/sendDimmer.js
+++ examples/sendDimmer.js
@@ -1,19 +1,23 @@
-var enocean      = require("../")();           // require node-enocean
-var Dimmer      = require("../node_modules/node-enocean-dimmer"); 
-enocean.listen("/dev/ttyUSB0");              // open the serialport
+// call this from the command line with command line parameters
+// "node sendDimmer.js teach" to send a learn telegram to an eltako dimming module
+// "node sendDimmer.js off" to switch of the Light
+// "node sendDimmer.js 50" to dimm the light 50%. you can pass any value between 0 and 100
+var enocean      = require( "../" )( ) // require node-enocean
+var Dimmer       = require( "node-enocean-dimmer" ) 
+enocean.listen( "/dev/ttyUSB0" ) // open the serialport
 
-enocean.on("ready",function(){           // when ready
+enocean.on( "ready" , function( ) {  
 	var dimmer = new Dimmer( enocean , 2 )
-	dimmer.speed = "01"
-	switch(process.argv[2]){
-	case "teach":
-		dimmer.teach();
-	break;
+	dimmer.speed = "01" // dimm very fast
+	switch( process.argv[ 2 ] ) {
+	case "teach" :
+		dimmer.teach( ) // 
+	break
 	case "off":
-		dimmer.off();
-	break;
+		dimmer.off( )
+	break
 	default:
-		dimmer.setValue(process.argv[2]);
+		dimmer.setValue( process.argv[ 2 ] )
 	break;
 	}
 	enocean.close()
