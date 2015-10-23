f10b572a450bd599d178b56fecdb4a17dc3ea081
Holger Will <h.will@klimapartner.de>
2015-10-23 14:16:47 +0200

    add test for a5-04-01 and a5-04-02

--- test/eeps.js
+++ test/eeps.js
@@ -114,7 +114,7 @@ describe('EEP Resolver', function() {
 		})
 	});
 	describe('a5-07-01', function() {	
-		it('should interpret "00" to be "open"', function () {
+		it('should return the "supported" "on" the PIR Status and Voltage', function () {
 			var dat=en.getData('a5-07-01','c307ff09')
 			assert.equal(dat[0].value,"supported")
 			assert.equal(dat[1].value,"on")
@@ -124,5 +124,42 @@ describe('EEP Resolver', function() {
 			assert.equal(dat[2].type,"Voltage")
 			assert.equal(dat[2].unit,"V")
 		})
+	});
+	describe('a5-04-xx', function() {	
+
+		function pad(num,size) {
+			var s = "00000000000000000000000000000000" + num;
+			return s.substr(s.length-size);
+		}
+		function getV(minB,maxB,minV,maxV,val1){
+			var B=((maxV-minV)/(maxB-minB))
+			val0   = ((val1-minV)/B)+minB
+			return pad(Math.floor(val0).toString(16),2)
+		}
+		var sensors={
+			"01":{hmin:0,hmax:100,tmin:0,tmax:40}, //1
+			"02":{hmin:0,hmax:100,tmin:-20,tmax:60}//2
+		}
+		for(var i=0;i<10;i++){
+			it('should return the correct dec for any random hex value (run '+i+')', function () {
+				for(var index in sensors){
+					var hmin = sensors[index].hmin
+					var hmax = sensors[index].hmax
+					var hv1 = Math.round(Math.random()*(hmax-hmin))+hmin
+					var hv0 = getV(0,255,hmin,hmax,hv1)
+					var tmin = sensors[index].tmin
+					var tmax = sensors[index].tmax
+					var tv1 = Math.round(Math.random()*(tmax-tmin))+tmin
+					var tv0 = getV(0,255,tmin,tmax,tv1)
+					var gd=en.getData('a5-04-'+index,'00'+hv0+tv0+'02')
+					assert.equal(gd.length,2)
+					assert.equal(Math.round(gd[0].value),hv1);
+					assert.equal(Math.round(gd[1].value),tv1);
+					var gd=en.getData('a5-04-'+index,'00'+hv0+tv0+'00')
+					assert.equal(gd.length,1)
+					assert.equal(Math.round(gd[0].value),hv1);
+				}
+			});
+		}
 	});
 });
