b86f99f77243b0fa94abf40a0f8519927b739592
Holger Will <h.will@klimapartner.de>
2016-08-16 12:42:00 +0200

    add a example for listening to VLD Telegrams

---
+++ examples/basic_vld.js
@@ -0,0 +1,15 @@
+var enocean = require( "../" )();  // require node-enocen
+// start listening on the serialport
+// if you use the enocean pi on a raspberry pi use:
+// enocean.listen("/dev/ttyAMA0")
+// on Windows use:
+// enocean.listen("COM1") or whatever COM-port you are connected
+enocean.listen( "/dev/ttyUSB0" );
+
+enocean.on( "data" , function( data ) {   // a telegram has been received
+
+  if(data.choice=="d2"){
+  	console.log( data )               // log it to the console
+  }
+
+});
