59a6bbd862cc00aca7b20477a6f070aae1692c1c
Holger Will <h.will@klimapartner.de>
2016-08-16 09:38:10 +0200

    fix typo

--- examples/send.js
+++ examples/send.js
@@ -2,15 +2,15 @@
 // "node send.js on" sends a switch on command. it can b eused as a teach in command
 // "node send.js off" sends a switch off command
 var enocean = require( "../" )( )             // require node-enocean
-var Button = require( "node-enocean-button" ) 
+var Button = require( "node-enocean-button" )
 enocean.listen( "/dev/ttyUSB0" );              // open the serialport
 
 enocean.on( "ready" , function( base ) {           // when ready
 	var button = new Button( enocean , 1 )       // use enocean to sen on chanal 1
-	if(p rocess.argv[ 2 ] == "on" ) {             
+	if(process.argv[ 2 ] == "on" ) {             
 		button.A1.click( )
 	}else{
-		button.A0.click( )	
+		button.A0.click( )
 	}
 	enocean.close( )
 });
