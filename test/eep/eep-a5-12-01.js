b91402a3ad5bbba5e19991b2c03fdc9ede5fb39f
Holger Will <h.will@klimapartner.de>
2016-08-20 11:42:12 +0200

    fix test for eep a5-12-01 to use the new multi telegram feature

--- test/eep/eep-a5-12-01.js
+++ test/eep/eep-a5-12-01.js
@@ -7,21 +7,12 @@ describe('a5-12-01', function() {
       var raw="0114da09"
       var gd = en.getData('a5-12-01',raw)
       expect(gd[0].value).to.equal(7087.4)
-      expect(gd[1].value).to.equal(10)
-      expect(gd[2].value).to.equal(0)
-			raw="00000019"
-			var gd = en.getData('a5-12-01',raw)
-			expect(gd[2].value).to.equal(1)
-			//expect(gd[0].devisor).to.equal(10)
 
 			raw="00021c0c"
 			var gd = en.getData('a5-12-01',raw)
-			expect(gd[0].value).to.equal(540)
-      expect(gd[1].value).to.equal(1)
-      expect(gd[2].value).to.equal(0)
+			expect(gd[1].value).to.equal(540)
 			raw="9903018f"
-			var gd = en.getData('a5-12-01',raw)
-			expect(gd[0].value).to.equal("ignored")
+
 
 		});
 });
