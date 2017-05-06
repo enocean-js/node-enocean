c7de475ef266743b9d647f126113716587395d2d
Holger Will <h.will@klimapartner.de>
2017-05-06 07:25:29 +0200

    add test for f6-10-00

---
+++ test/eep/eep-f6-10-00.js
@@ -0,0 +1,12 @@
+var assert = require('chai').assert
+var expect = require('chai').expect
+var en = require("../../")();
+var Help = require("../../modules/eepGeneratorHelper.js")
+describe('f6-10-00', function() {
+				var gd = en.getData('f6-10-00',0b11000000.toString(16))
+				expect(gd[0].value).to.equal("handle horizontal")
+        var gd = en.getData('f6-10-00',0b11010000.toString(16))
+				expect(gd[0].value).to.equal("handle vertical up")
+        var gd = en.getData('f6-10-00',0b11110000.toString(16))
+				expect(gd[0].value).to.equal("handle vertical down")
+});
