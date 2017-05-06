ac698050382d1f3fd49515a625c7180954e19107
Holger Will <h.will@klimapartner.de>
2017-05-06 08:05:27 +0200

    add check for "the bits marked with x should not be checked..." f6-10-00

--- test/eep/eep-f6-10-00.js
+++ test/eep/eep-f6-10-00.js
@@ -5,6 +5,8 @@ var Help = require("../../modules/eepGeneratorHelper.js")
 describe('f6-10-00', function() {
 				var gd = en.getData('f6-10-00',0b11000000.toString(16))
 				expect(gd[0].value).to.equal("handle horizontal")
+        var gd = en.getData('f6-10-00',0b11100000.toString(16))
+				expect(gd[0].value).to.equal("handle horizontal")
         var gd = en.getData('f6-10-00',0b11010000.toString(16))
 				expect(gd[0].value).to.equal("handle vertical up")
         var gd = en.getData('f6-10-00',0b11110000.toString(16))
